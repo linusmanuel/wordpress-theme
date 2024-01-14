347b0c95be0f75e52a069784d01dd23091fd5bc8
Joao Lino <linusmannuel@gmail.com>
2024-01-14 12:52:19 +0100

    feature: Showing posts in home page

---
+++ single.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Single page template
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+    <?php 
+      while (have_posts()): 
+        the_post();
+        the_content();
+      endwhile;
+    ?>
+  </main>
+</div>
