f089889490d4cbf055d0e9adfa0050ebc8239ea1
Joao Lino <linusmannuel@gmail.com>
2024-01-14 17:32:33 +0100

    feature: implementing single.php

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -8,7 +8,11 @@
 <article id="post-<?php the_ID() ?>">
   <header class="entry-header">
     <?php 
-      the_title('<h2 class="entry-title"><a class="entry-link" href="'.esc_url( get_permalink()).'">', '</a></h2>')
+      if (is_singular()): 
+        the_title('<h1 class="entry-title">', '</h1>');
+      else:
+        the_title('<h2 class="entry-title"><a class="entry-link" href="'.esc_url( get_permalink()).'">', '</a></h2>');
+      endif;
     ?>
   </header>
 
@@ -21,7 +25,20 @@
   ?>
 
   <!-- Post Content -->
-  <div class="entry-content">
-    <?php the_excerpt ?>
-  </div>
+  <?php if( is_home() || is_archive()): ?>
+    <div class="entry-content">
+      <?php the_excerpt();?>
+    </div>
+  <?php elseif( is_single()): ?>
+    <div class="entry-content">
+      <?php 
+        the_content(); 
+
+        wp_link_pages( array (
+          'before' => '<div class="page-links">' . esc_html__('Pages:', 'ninestars'), 
+          'after' => '</div>',
+        ));
+      ?>
+    </div>
+  <?php endif; ?>
 </article>
