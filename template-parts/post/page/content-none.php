c433c6e4abb98ca222ba8ae18c19aee72ae968a0
Joao Lino <linusmannuel@gmail.com>
2024-01-19 07:13:33 +0100

    feature: Implementing content-none

---
+++ template-parts/post/page/content-none.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * Template part for displaying a message that posts cannot be found
+ */
+?>
+
+<section class="no-results no-found">
+  <header class="page-header">
+    <h1 class="page-title"><?php esc_html_e('Nothing Found', 'herobiz'); ?></h1>
+  </header>
+
+  <div class="page-content">
+    <?php 
+      if (is_home() && current_user_can('publish_posts')):
+        printf(
+          '<p>' . wp_kses (
+            /* translators: 1: link to WP admin new post page. */
+            __('Ready to publish your first post? <a href="%1$s">Get started here</a>', 'herobiz'),
+            array(
+              'a' => array(
+                'href' => array(),
+              ),
+            )
+          ) . '</p>', 
+          esc_url( admin_url('post-new.php'))
+        );
+      elseif(is_search()) :
+      ?>
+    <p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some differente word') ?>
+    </p>;
+    <?php 
+          get_search_form();
+
+      else: 
+        ?>
+    <p><?php esc_html_e('It seems we can&squo;t find what you&squo;re looking for. Perhaps searching') ?></p>
+    <?php 
+          get_search_form();
+    endif;
+    ?>
+  </div>
+</section>
