bb65c86599b981df91eddddbf53ef008791f0b09
Joao Lino <linusmannuel@gmail.com>
2024-01-31 07:00:28 +0100

    feature: implementing search

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 /**
  * 
  */
-
+get_search_form();
 get_header();
-  if( have_posts()): 
-    while (have_posts()) :
-      the_post();
-      get_template_part('template-parts/post/content');
-    endwhile;
-  endif;
+if (have_posts()) :
+  while (have_posts()) :
+    the_post();
+    get_template_part('template-parts/post/content');
+  endwhile;
+endif;
 get_footer();
---
+++ search.php
@@ -0,0 +1,41 @@
+<?php
+
+/** 
+ *  The template for displaying search pages
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+    <?php if (have_posts()) : ?>
+      <header class="archive-page-header">
+        <?php
+        the_archive_title('<h1 class="archive-title">', '</h1>');
+        the_archive_description('<div class="archive-description">', '</div>');
+        ?>
+      </header>
+
+      <?php
+      // Start the loop
+      while (have_posts()) :
+        the_post();
+        get_template_part('template-parts/post/content', 'search');
+      endwhile;
+
+      echo paginate_links([
+        'prev_text' => esc_html__('Prev', 'sks'),
+        'next_text' => esc_html__('next', 'sks')
+      ]);
+
+    else :
+      get_template_part('template-parts/post/content', 'none');
+      ?>
+    <?php endif ?>
+  </main>
+
+</div>
+
+<?php
+get_footer();
